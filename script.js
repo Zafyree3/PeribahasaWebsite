6d41d74f68db648c6ecbf462cd1a3cc9659b83ed
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2022-05-29 22:37:12 +0800

    Optimization & Mobile Ver

--- script.js
+++ script.js
@@ -4,55 +4,55 @@ option1HTML = document.getElementById("option1");
 option2HTML = document.getElementById("option2");
 option3HTML = document.getElementById("option3");
 option4HTML = document.getElementById("option4");
+optHTMLArr = [option1HTML,option2HTML,option3HTML,option4HTML]
+colors = ["#457b9d","#a8dadc","#f1faee","#ffd6e0"]
 counter = 0;
 correct = 0;
+wrongpage = 0;
 
 function randomNum(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 function call() {
-    alert(correct);
+    // fetch("data.json")
+    // .then(response => response.json())
+    // .then(jsondata => { 
+    //     console.log(jsondata[9].maksud[1]);
+    // });
+    //console.log(typeof randomNum(0,1));
 }
-function select1() {
-    if (correct == 1) {
-        counter++;
-        randomise();
-    }
-    else {
+
+function select(option){
+    if (wrongpage == 0){
+        if (option == correct) {
+            counter++;
+            randomise();
+        } else {
+            wrongpage = 1;
+            wrong();
+        }
+    } else {
+        wrongpage = 0;
         counter = 0;
-        randomise();
-    }
-}
-function select2() {
-    alert(correct)
-    if (correct == 2) {
-        counter++;
-        randomise();
-    }
-    else {
-        counter = 0;
-        randomise();
-    }
-}
-function select3() {
-    if (correct == 3) {
-        counter++;
-        randomise();
-    }
-    else {
-        counter = 0;
+        reset();
         randomise();
     }
 }
-function select4() {
-    if (correct == 4) {
-        counter++;
-        randomise();
+
+function wrong() {
+    for (var i = 0; i < 4; i++) {
+        if (i+1 == correct) {
+            optHTMLArr[i].parentElement.style.backgroundColor = "#90cf8e";
+        } else {
+            optHTMLArr[i].parentElement.style.backgroundColor = "#ee6055";
+        }
     }
-    else {
-        counter = 0;
-        randomise();
+}
+
+function reset() {
+    for (var i = 0; i < 4; i++) {
+        optHTMLArr[i].parentElement.style.backgroundColor = colors[i];
     }
 }
 
@@ -74,24 +74,22 @@ function randomise() {
     .then(jsondata => { 
         var result = jsondata;
         var options = get4DiffNum(result.length - 1);
-        var correct = randomNum(1,4);
-        alert(correct);
+        correct = randomNum(1,4);
+        //alert(correct);
         peribahasaHTML.innerHTML = result[options[correct-1]].peribahasa;
         for (var i = 0; i < 4; i++) {
             var option = options[i];
             var optionText = result[option];
-            switch (i+1) {
-                case 1:
-                    option1HTML.innerHTML = optionText.maksud;
-                case 2:
-                    option2HTML.innerHTML = optionText.maksud;
-                case 3:
-                    option3HTML.innerHTML = optionText.maksud;
-                case 4:
-                    option4HTML.innerHTML = optionText.maksud;
+            //console.log("option" + i + ": " + typeof optionText.maksud)
+            //console.log(typeof optionText.maksud == "object")
+            if (typeof optionText.maksud == "object") {
+                optHTMLArr[i].innerHTML = optionText.maksud[randomNum(1,3)]
+            } else{
+                optHTMLArr[i].innerHTML = optionText.maksud;
             }
                     //alert("error")
         }
+        ///alert(correct);
         // var index = randomNum(0, result.length - 1);
         // var data = result[index];
         // peribahasaHTML.innerHTML = data.peribahasa;
