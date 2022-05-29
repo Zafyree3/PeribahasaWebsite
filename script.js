8e78ea22fa3a1b42acb5327675bb029f03f3fb9a
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2022-05-29 21:28:21 +0800

    V2

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function select1() {
     }
 }
 function select2() {
+    alert(correct)
     if (correct == 2) {
         counter++;
         randomise();
@@ -55,91 +56,122 @@ function select4() {
     }
 }
 
+function get4DiffNum(max) {
+    var num = [];
+    while (num.length < 4) {
+        var newNum = randomNum(0, max);
+        if (!num.includes(newNum)) {
+            num.push(newNum);
+        }
+    }
+    return num;
+}
+
 function randomise() {
     counterHTML.innerHTML= "Correct Combo: " + counter;
     fetch("data.json")
     .then(response => response.json())
     .then(jsondata => { 
         var result = jsondata;
-        var index = randomNum(0, result.length - 1);
-        var data = result[index];
-        peribahasaHTML.innerHTML = data.peribahasa;
-        if (randomNum(1, 4) == 1) { 
-            correct = 1;
-            option1HTML.innerHTML = data.maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
+        var options = get4DiffNum(result.length - 1);
+        var correct = randomNum(1,4);
+        alert(correct);
+        peribahasaHTML.innerHTML = result[options[correct-1]].peribahasa;
+        for (var i = 0; i < 4; i++) {
+            var option = options[i];
+            var optionText = result[option];
+            switch (i+1) {
+                case 1:
+                    option1HTML.innerHTML = optionText.maksud;
+                case 2:
+                    option2HTML.innerHTML = optionText.maksud;
+                case 3:
+                    option3HTML.innerHTML = optionText.maksud;
+                case 4:
+                    option4HTML.innerHTML = optionText.maksud;
             }
-            option2HTML.innerHTML = result[newIndex].maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option3HTML.innerHTML = result[newIndex].maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option4HTML.innerHTML = result[newIndex].maksud;
+                    //alert("error")
         }
-        else if (randomNum(1, 4) == 2) {
-            correct = 2;
-            option2HTML.innerHTML = data.maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option1HTML.innerHTML = result[newIndex].maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option3HTML.innerHTML = result[newIndex].maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option4HTML.innerHTML = result[newIndex].maksud;
-        }
-        else if (randomNum(1, 4) == 3) {
-            correct = 3;
-            option3HTML.innerHTML = data.maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option2HTML.innerHTML = result[newIndex].maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option1HTML.innerHTML = result[newIndex].maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option4HTML.innerHTML = result[newIndex].maksud;
-        }
-        else {
-            correct = 4;
-            option4HTML.innerHTML = data.maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option2HTML.innerHTML = result[newIndex].maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option3HTML.innerHTML = result[newIndex].maksud;
-            newIndex = randomNum(0, result.length - 1);
-            while (newIndex == index) {
-                newIndex = randomNum(0, result.length - 1);
-            }
-            option1HTML.innerHTML = result[newIndex].maksud;
-        }
+        // var index = randomNum(0, result.length - 1);
+        // var data = result[index];
+        // peribahasaHTML.innerHTML = data.peribahasa;
+        // if (randomNum(1, 4) == 1) { 
+        //     correct = 1;
+        //     option1HTML.innerHTML = data.maksud;
+        //     alert((typeof data.maksud));
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option2HTML.innerHTML = result[newIndex].maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option3HTML.innerHTML = result[newIndex].maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option4HTML.innerHTML = result[newIndex].maksud;
+        // }
+        // else if (randomNum(1, 4) == 2) {
+        //     correct = 2;
+        //     option2HTML.innerHTML = data.maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option1HTML.innerHTML = result[newIndex].maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option3HTML.innerHTML = result[newIndex].maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option4HTML.innerHTML = result[newIndex].maksud;
+        // }
+        // else if (randomNum(1, 4) == 3) {
+        //     correct = 3;
+        //     option3HTML.innerHTML = data.maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option2HTML.innerHTML = result[newIndex].maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option1HTML.innerHTML = result[newIndex].maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option4HTML.innerHTML = result[newIndex].maksud;
+        // }
+        // else {
+        //     correct = 4;
+        //     option4HTML.innerHTML = data.maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option2HTML.innerHTML = result[newIndex].maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option3HTML.innerHTML = result[newIndex].maksud;
+        //     newIndex = randomNum(0, result.length - 1);
+        //     while (newIndex == index) {
+        //         newIndex = randomNum(0, result.length - 1);
+        //     }
+        //     option1HTML.innerHTML = result[newIndex].maksud;
+        // }
     });
 }
 randomise();
